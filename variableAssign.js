ee4a47358b8b335743bebbfb5b22ddbab42bcbf4
vishallengare61 <vishallengare61@gmail.com>
2023-02-16 12:52:13 +0530

    Complete the Assessment on JavaScript on 16 th Feb Lecture.
    Complete the Assessment on JavaScript on 16 th Feb Lecture.

---
+++ variableAssign.js
@@ -0,0 +1,35 @@
+
+console.log("--------------Before Initilizatin--------------");
+var myFullName;
+console.log(myFullName);
+
+console.log("--------------After Initilizatin--------------");
+var myFullName = "Vishal Lengare";
+console.log(myFullName);
+
+console.log("-------------- Updated Full Name --------------"); 
+
+var myFullName = "Vishal Birudev Lengare";
+console.log(myFullName);
+
+console.log("-------------- Declaration And Initialization variable --------------");  
+
+var myLove = "I Love Only JS";
+console.log(myLove);
+
+
+console.log("-------------- Personal Details  --------------");  
+
+var firstName = "Vishal";
+var lastName = "Lengare";
+var age = "21";
+var address = "Sangola";
+var pincode = "413306";
+var gender = "male";
+var isMarried = "UunMarried";
+
+console.log("-----------------------------------------------------------------------");
+console.log("firstName | lastName | age | address | pincode | gender | isMarried");
+console.log(firstName ,"   |", lastName ," |", age ," |", address ,"|", pincode ," |", gender ," |", isMarried);
+console.log("-----------------------------------------------------------------------"); 
+
