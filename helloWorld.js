df14fd34a6d560ef02e0d7f71cf381061c38cf9e
vishallengare61 <vishallengare61@gmail.com>
2023-02-16 08:43:23 +0530

    Updade the code according to 16th feb lecture.
    Update the code according to 16th feb lecture.

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,14 @@
 console.log("Vishal Lengare");
+console.log("31");
+console.log("Sangola");
+
+var fullName = "Vishal Lengare";
+console.log(fullName);
+console.log(fullName);
+
+var city;
+console.log(city);
+city = "Pune";
+console.log(city);
+city = "Mumbai";
+console.log(city);
