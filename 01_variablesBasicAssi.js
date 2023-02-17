8628f8221f478bcf4ad167c3b6fb88ef6dcc5725
vishallengare61 <vishallengare61@gmail.com>
2023-02-17 11:46:34 +0530

    Create variableBasicAssig.js file as per assignment first on 17 feb lecture.
    Create variableBasicAssig.js file as per assignment first on 17 feb lecture.

---
+++ 01_variablesBasicAssi.js
@@ -0,0 +1,20 @@
+
+var PIN =  413306;
+var name = "Vishal";
+var married = false;
+
+console.log("PIN :" , PIN , "type:", typeof PIN);
+console.log("Name :" , name , "type:", typeof name);
+console.log("married status :" , married , "type:", typeof married);
+
+
+console.log("**************************************************************");
+
+console.log("******Differance Between null & undefined*******");
+
+
+console.log("Null :-");
+console.log("Whenever we declaire a variable and initilaize it with null value these variable is known as NULL.");
+
+console.log("undefined :-");
+console.log("Whenever we just declare a variable and not initilize it and try to print this variable then it will print null as a output.");
