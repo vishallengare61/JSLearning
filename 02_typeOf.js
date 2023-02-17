39d33f64f66faa440a5be33b2cb3ca765d4c083e
vishallengare61 <vishallengare61@gmail.com>
2023-02-17 12:43:52 +0530

    Create typeOf.js file as per assignment second on 17 feb lecture.
    Create typeOf.js file as per assignment second on 17 feb lecture.

---
+++ 02_typeOf.js
@@ -0,0 +1,31 @@
+
+var bankName;
+console.log("Type of bankNane before intilizing :" ,bankName);
+bankName  = "SBI bank";
+console.log("Type of bankNane after initilizing :" ,bankName);
+
+console.log("**********************************");
+var marks = '90%';
+console.log("your Marks are :", marks," And the data type of marks is :",typeof marks);
+
+console.log("**********************************");
+
+var isWorking;
+
+console.log("The type of isworking is before intilizing :" , typeof isWorking);
+
+isWorking = true;
+
+console.log("type of isWorking after initilizing : " ,typeof isWorking);
+
+console.log("Are you curruntly working :", isWorking);
+
+console.log("**********************************");
+
+var totalCount ="hundred and seven";
+
+console.log("The Total Count is :", totalCount  ,"And the type of totalCount is :", typeof totalCount);
+
+totalCount = 107;
+
+console.log("After Overriding the total count the count is :", totalCount ,"And the type is :", typeof totalCount);
