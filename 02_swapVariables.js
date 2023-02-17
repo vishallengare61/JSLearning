e70228524f733f3876c944bb6f94b539ccfd46aa
vishallengare61 <vishallengare61@gmail.com>
2023-02-17 12:57:41 +0530

    Create swapVariables.js file as per assignment third on 17 feb lecture.
    Create swapVariables.js file as per assignment third on 17 feb lecture.

---
+++ 02_swapVariables.js
@@ -0,0 +1,40 @@
+console.log("------------------FIRST TASK------------------");
+var sweety = "Sweety";
+var cutie = "Cutie";
+
+console.log("**********Before swaping***************");
+console.log("The value of sweety Before swapping :",sweety);
+console.log("The value of cutie Before swapping :",cutie);
+console.log("**********After swaping***************");
+
+var temp = sweety;
+
+sweety = cutie;
+cutie =  temp;
+ console.log("the value of Sweety after swapping =" ,sweety);
+console.log(" the value of Cuite after swapping =", cutie);
+
+console.log("------------------SECOND TASK------------------");
+
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+
+console.log("**********Before swaping***************");
+
+console.log("The value of num1 Before swapping :",num1);
+console.log("The value of num2 Before swapping :",num2);
+console.log("The value of num3 Before swapping :",num3);
+
+console.log("**********After swaping***************");
+
+var temp = num1;
+
+num1 = num2;
+num2 = num3;
+num3 = temp;
+
+console.log("The value of num1 after swapping :",num1);
+console.log("The value of num2 after swapping :",num2);
+console.log("The value of num3 after swapping :",num3);
+
