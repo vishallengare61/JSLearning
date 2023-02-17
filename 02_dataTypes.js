f8eaf66a436ced0e62fb511ae43ee65149c4eb07
vishallengare61 <vishallengare61@gmail.com>
2023-02-17 11:32:43 +0530

    update the file according to data type in live lecture.
    update the file according to data type in live lecture.

---
+++ 02_dataTypes.js
@@ -0,0 +1,24 @@
+var myName = 'Virat Kohli';
+var fatherName = "Yogesh Kohli"
+
+var age = 31;
+var weight = 82.5;
+
+var isMarried = true;
+var isLazy = false; 
+
+var typeOfMyName  = typeof myName;
+console.log("Typeof variable myName: ",  typeOfMyName);
+console.log("typeOf variable fatherName:", typeof fatherName);
+
+console.log("Typeof variable age: ",  typeof  age);
+console.log("Typeof variable weight: ",  typeof  weight);
+console.log("Typeof variable isMarried status: ",  typeof  isMarried);
+
+var graduation;
+console.log("Value of  graduation variable is: ", graduation);
+console.log("typeOf graduation is: ",typeof graduation);
+
+
+var alphabate = 'a';
+console.log(typeof alphabate);
