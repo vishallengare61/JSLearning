004078c5b61854927781f2c7ec5621442f077260
vishallengare61 <vishallengare61@gmail.com>
2023-02-22 12:03:16 +0530

    Created js files as per 22 feb lecture.
    Created js files as per 22 feb lecture.

---
+++ 04_srteingTemplate.js
@@ -0,0 +1,10 @@
+console.log(`you are "champ" man`);
+
+
+
+var fname = "Vishal";
+var lname = "Lengare";
+var city = "Pune";
+console.log("My First name ", fname, "Last name ", lname, " City ", city);
+
+console.log(`My first name is ${fname}   last name is  ${lname} "and city is ${city}`);
---
+++ 05_operator.js
@@ -0,0 +1,38 @@
+
+
+
+var num1 = 10;
+
+var num2 = 2;
+var result = num1 + num2; // Addition
+console.log(` Addition is ${result}`);
+
+result = num1 - num2;
+console.log(`Subtraction is  ${result}`);
+
+result = num1 * num2;
+console.log(`Multiplication is  ${result}`);
+
+ result = num1 ** num2;
+ console.log(`Exponential is  ${result}`);
+
+ result = num1 ** num2;
+ console.log(`Exponential is  ${result}`);
+
+ result = num1 / 3;
+ console.log(` Division is ${result} `);
+
+ result = num1 % 3; // Modulus
+ console.log(` Reminder is ${result} `);
+
+ var num = 10; // Compound addition +=
+ num+=20; // num = num + 20
+ console.log(`Compound addition += ${num}`);
+
+
+ console.log(`Caparison operators `);
+ var num3 = "10";
+ var num4 = 10;
+ console.log(num3==num4); // 10==10
+ console.log(num3===num4);
+ 
