8219281f04972c8e714fdbfc4df18db517d447d0
vishallengare61 <vishallengare61@gmail.com>
2023-02-24 13:46:33 +0530

    Example of NaN (Not a Number) .
    Example of NaN (Not a Number) .

---
+++ 07_NaN.js
@@ -0,0 +1,13 @@
+var num1 = "Vishal";
+var num2 = 10;
+
+console.log(num1-num2);
+console.log("-------------------------");
+
+console.log(0/0);
+
+console.log("-------------------------");
+
+var fullName = "Hello";
+var myNumber = +fullName;
+console.log(myNumber);
