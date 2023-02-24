d6e873c0b74d1298c29345f7b5f773f3980752c3
vishallengare61 <vishallengare61@gmail.com>
2023-02-24 13:44:44 +0530

    Checks the difference between '==' & '===' operators.
    Checks the difference between '==' & '===' operators.

---
+++ 07_diffBet==_===.js
@@ -0,0 +1,12 @@
+
+
+
+var num1 = 10;
+var num2 = "10";
+
+console.log(num1==num2);
+//JS will try to convert them to the same type and then compare them  (Type conversion)
+
+
+console.log(num1===num2);
+//it checks whether two numbers are equal by it’s value and type as well.
