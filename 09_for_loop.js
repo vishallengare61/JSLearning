cb9a979915d9962925a74af249b3a8241eddf77c
vishallengare61 <vishallengare61@gmail.com>
2023-03-01 10:36:06 +0530

    learn the for loop on lecture1 march
    learn the for loop on lecture1 march

---
+++ 09_for_loop.js
@@ -0,0 +1,31 @@
+
+
+
+for (let index = 1; index  <=10; index=index+1) {
+    console.log(index);
+    
+}
+
+
+
+console.log("Reverse number");
+
+
+for (let index = 10; index >= 0; index = index-1) {
+    console.log(index);
+    
+}
+
+console.log("Table of five");
+
+for (let index = 5; index <= 50; index=index+5) {
+    console.log(index);
+    
+}
+
+console.log("infinite loop");
+
+for (let index = 0; index <=10;) {
+    console.log(index);
+    
+}
