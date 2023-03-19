fbdc7854096142c7dd06c80849722df968c282f7
vishallengare61 <vishallengare61@gmail.com>
2023-03-19 15:26:40 +0530

    Learn how to call back.
    Learn how to call back.

--- callback.js
+++ callback.js
@@ -10,3 +10,21 @@ function copyAssignment(){
     console.log("Ohh finally coped all assignment");
 }
 doAssignment(copyAssignment);
+
+
+
+function greet(){
+    console.log("Good Morning.....");
+}
+setTimeout(greet, 4000);
+
+
+function firstClass(){
+    console.log("We can return function from another function");
+    return function(){
+        console.log("Inner function");
+    }
+    // return innerFunction;
+}
+const inner = firstClass();
+inner();
